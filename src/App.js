491c6d03452435e6c14d1d0c00bc857c8faff44c
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-21 19:32:09 +0600

    react

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const number = 5555;
-const singer = { name: "dr. Mahfuz", job: "singer" };
-const singer2 = { name: "Eva Rohoman", job: "singer2" };
+const singers = [
+  { name: "dr. Mahfuz", job: "singer" },
+  { name: "Eva Rohoman", job: "singer2" },
+  { name: "Agun", job: "sopno" },
+  { name: "Shuvro", job: "pathor" },
+];
 
 const singerStyle = {
   color: "purple",
@@ -11,34 +15,53 @@ const singerStyle = {
 };
 
 function App() {
+  const nayoks = [
+    "Rubel",
+    "BappaRaz",
+    "Kuber",
+    "Jashim",
+    "Salman Shah",
+    "Riyaz",
+  ];
   return (
     <div className="App">
-      <Parson></Parson>
-      <Parson></Parson>
-      <Parson></Parson>
+      {nayoks.map((nayok) => (
+        <li>Name: {nayok}</li>
+      ))}
+
+      {nayoks.map((nayok) => (
+        <Parson name={nayok}></Parson>
+      ))}
+      {singers.map((singer) => (
+        <Parson name={singer.name}></Parson>
+      ))}
+
+      {/* <Parson name={nayoks[0]} nayika="Mousumi"></Parson>
+      <Parson name={nayoks[1]} nayika="Cheka"></Parson>
+      <Parson name={nayoks[2]} nayika="Kopila"></Parson> */}
       <h5>New component. YAY</h5>
       <p>rock mama React mama.</p>
-      <Friend></Friend>
-      <Friend></Friend>
+      <Friend movie="Singgam" phone="0177777"></Friend>
+      <Friend phone="0199999"></Friend>
     </div>
   );
 }
 
 function Parson(props) {
-  console.log(props);
   return (
     <div className="parson">
-      <h1>Sakib Al Hasan</h1>
-      <p>profession: Cricket</p>
+      <h1>{props.name}</h1>
+      <p>{props.nayika}</p>
     </div>
   );
 }
 
-function Friend() {
+function Friend(props) {
+  // console.log(props);
   return (
     <div className="container">
-      <h3>Name: Ajay Devgun</h3>
-      <p>Job: mara mari</p>
+      <h3>Name: {props.movie}</h3>
+      <p>Phone: {props.phone}</p>
     </div>
   );
 }
