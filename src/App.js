61658bdd18e25076d3beff0d36a664387df82033
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-21 13:53:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
