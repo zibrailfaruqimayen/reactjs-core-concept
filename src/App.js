858df046d882f9e9e66967498a9e4675c77599b8
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-21 16:26:07 +0600

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+const number = 5555;
+const singer = { name: "dr. Mahfuz", job: "singer" };
+const singer2 = { name: "Eva Rohoman", job: "singer2" };
+
+const singerStyle = {
+  color: "purple",
+  backgroundColor: "white",
+};
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parson></Parson>
+      <Parson></Parson>
+      <Parson></Parson>
+      <h5>New component. YAY</h5>
+      <p>rock mama React mama.</p>
+      <Friend></Friend>
+      <Friend></Friend>
+    </div>
+  );
+}
+
+function Parson(props) {
+  console.log(props);
+  return (
+    <div className="parson">
+      <h1>Sakib Al Hasan</h1>
+      <p>profession: Cricket</p>
+    </div>
+  );
+}
+
+function Friend() {
+  return (
+    <div className="container">
+      <h3>Name: Ajay Devgun</h3>
+      <p>Job: mara mari</p>
     </div>
   );
 }
